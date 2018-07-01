b8b3846768e83eecd63e34618da290c55a7b250b
Nuno Carriço <nuno192009@live.com.pt>
2018-07-01 23:45:27 +0100

    Dialog chain

--- modules/dialogChain.js
+++ modules/dialogChain.js
@@ -19,8 +19,30 @@ class DialogChain {
 
         let dialogToChange = this.dialogs[fase];
 
+        this.channel.send(dialogToChange.question);
+
         dialogToChange.setAnswer(answer);
     }
 
-    
+    initDialog() {
+
+        let collector = this.channel.createMessageCollector(m => m !== undefined);
+
+        collector.on('collect', (m) => {
+
+            do {
+                
+                this.channel.send(this.dialogs[this.fase].question);
+
+                this.dialogs[this.fase].setAnswer(m.content);
+
+                this.fase++;
+
+            } while (m.content.match(/done/i) === null && this.fase < this.dialogs.length);
+
+            collector.stop();
+
+            this.fase = 0;
+        })
+    }
 }
