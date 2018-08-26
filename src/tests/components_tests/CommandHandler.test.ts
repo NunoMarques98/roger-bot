671c51b12003cc7cdd7a2c32e2463b66ec762d8b
Nuno Carriço <nuno192009@live.com.pt>
2018-08-27 00:03:05 +0100

    More tests to command handler

--- src/tests/components_tests/CommandHandler.test.ts
+++ src/tests/components_tests/CommandHandler.test.ts
@@ -1,28 +1,25 @@
 import * as Discord from "discord.js";
 import { checkCommands, getCommandParts, handleCommands } from "../../components/CommandHandler";
 import { token } from "../../../settings.json";
+import { createHash } from "crypto";
 
 let message : Discord.Message;
 let messageWithWrongCommand : Discord.Message;
+let emptyMessage : Discord.Message;
 let channel : Discord.TextChannel;
 let client : Discord.Client = new Discord.Client();
 
 beforeAll( async () => {
 
     await client.login(token);
-})
-
-beforeEach(async () => {
 
     channel = <Discord.TextChannel> client.channels.filter(channel => channel.type === "text").first();
 
     message = await <any> channel.send("$deadline -o Absolute");
     messageWithWrongCommand = await <any> channel.send("$pl test");
-})
+    emptyMessage = await <any> channel.send("Empty");
 
-afterEach(() => {
-
-    
+    emptyMessage.content = "";
 })
 
 afterAll( () => {
@@ -30,15 +27,27 @@ afterAll( () => {
     client.destroy();
 })
 
-test("Separating command parts test", () => {
+describe("Command handling tests", () => {
 
-    let parts : Array<string> = getCommandParts(message);
+    test("Getting command parts test with non empty message", () => {
 
-    expect(parts).toHaveLength(3);
-    expect(Array.isArray(parts)).toBe(true);
+        let parts : Array<string> = getCommandParts(message);
+    
+        expect(parts).toHaveLength(3);
+        expect(Array.isArray(parts)).toBe(true);
+    })
+
+    test("Getting command parts with empty message", () => {
+
+        let parts : Array<string> = getCommandParts(emptyMessage);
+
+        expect(parts).toHaveLength(1);
+        expect(Array.isArray(parts)).toBe(true);
+    })
+    
+    test("Command calling test", () => {
+    
+        expect( () => { checkCommands(messageWithWrongCommand) }).toThrowError("Command not found!")
+    })
 })
 
-test("Command calling", () => {
-
-    expect( () => {checkCommands(messageWithWrongCommand)}).toThrowError()
-})
