dccaf3b43f73d547047e61caa71682d0cca09506
Nuno Carriço <nuno192009@live.com.pt>
2018-08-27 00:29:54 +0100

    Dialog tests

---
+++ src/tests/components_tests/Dialog.test.ts
@@ -0,0 +1,29 @@
+import Dialog from "../../components/Dialog";
+
+let dialog : Dialog;
+
+beforeAll( () => {
+
+    dialog = new Dialog("Test dialog");
+})
+
+describe("Dialog tests", () => {
+
+    test("Property testing", () => {
+
+        expect(dialog).toHaveProperty("answer");
+        expect(dialog).toHaveProperty("question");
+    })
+
+    test("Dialog with no answer", () => {
+
+        expect(dialog.answer).toBeNull();
+    })
+
+    test("Dialog with answer", () => {
+
+        dialog.setAnswer("test");
+        
+        expect(dialog.answer).not.toBeNull();
+    })
+})
